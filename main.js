5e4cb9cb663daed1acf5a301a4ce6febe794c238
pietro siciliano <siciliano.pietro@gmail.com>
2020-07-29 17:55:15 +0200

    k

--- main.js
+++ main.js
@@ -1,3 +1,41 @@
-document.addEventListener("DOMContentLoaded", () => {
-    
-})
+$(document).ready(function() {
+
+    $("footer input").keydown(aggiungi);
+
+    function aggiungi(){
+        if(event.which == 13){
+            var valore = $("footer input").val();
+            $("footer input").val("");
+            var copia = $(".template .out").clone();
+            copia.find(".text").append("<p>"+valore+"</p>")
+            $("main").append(copia);
+
+            setTimeout(reply, 1000)
+
+            function reply(){
+
+                var answerArr = [
+                    "ciao",
+                    "come stai",
+                    "a presto"
+                ]
+
+                var answer = $(".template .in").clone();
+                answer.find(".text").append("<p>"+ answerArr[numRand(0,2)] +"</p>")
+                $("main").append(answer);
+            }
+        };
+    }
+
+    function numRand(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+    }
+
+
+
+    var data = new Date();
+    var ora = data.getHours() + ":" + data.getMinutes();
+
+
+
+});
