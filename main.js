a17592c59c5fecf62117dd2d834f6d31fbc9287b
pietro siciliano <siciliano.pietro@gmail.com>
2020-07-30 00:39:22 +0200

    k

--- main.js
+++ main.js
@@ -4,8 +4,10 @@ $(document).ready(function() {
         var valore = $("footer input").val();
         $("footer input").val("");
         var copia = $(".template .out").clone();
-        copia.find(".text").append("<p>"+valore+"</p>");
+        copia.find(".text").append("<p>" + valore + "</p>");
         $("main").append(copia);
+        var x = $("main .out .text").find("p").attr("data-descr", orario());
+        scrollChat();
         setTimeout(reply, 1000);
     })
 
@@ -15,12 +17,14 @@ $(document).ready(function() {
         $("footer .speak i:nth-of-type(1)").hide();
         $("footer .speak i:nth-of-type(2)").show();
     })
+
     $("footer input").focusout(function(){
         $("footer .speak i:nth-of-type(1)").show();
         $("footer .speak i:nth-of-type(2)").hide();
     })
 
     $("aside .chat .text .name").append("<span>" + orario() + "</span>");
+
     $("header .contact").find("span:nth-of-type(2)").append(orario());
 
     function aggiungi(){
@@ -30,9 +34,15 @@ $(document).ready(function() {
             var copia = $(".template .out").clone();
             copia.find(".text").append("<p>"+valore+"</p>");
             $("main").append(copia);
-
+            var x = $("main .out .text").find("p").attr("data-descr", orario());
+            scrollChat();
             setTimeout(reply, 1000)
         };
+    }
+
+    function scrollChat(){
+        $('main').animate({
+            scrollTop: $('main')[0].scrollHeight});
     }
 
     function reply(){
@@ -46,6 +56,8 @@ $(document).ready(function() {
         var answer = $(".template .in").clone();
         answer.find(".text").append("<p>"+ answerArr[numRand(0,2)] +"</p>");
         $("main").append(answer);
+        var x = $("main .in .text").find("p").attr("data-descr", orario());
+        scrollChat();
     }
 
     function numRand(min, max) {
