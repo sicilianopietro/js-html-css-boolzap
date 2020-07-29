016f39e4465944dd43b3129ec3c6d61091c75d95
pietro siciliano <siciliano.pietro@gmail.com>
2020-07-29 22:50:59 +0200

    k

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
 $(document).ready(function() {
 
+    $(".speak").click(function(){
+        var valore = $("footer input").val();
+        $("footer input").val("");
+        var copia = $(".template .out").clone();
+        copia.find(".text").append("<p>"+valore+"</p>")
+        $("main").append(copia);
+        setTimeout(reply, 1000);
+    })
+
     $("footer input").keydown(aggiungi);
+
+    $("footer input").focus(function(){
+        $("footer .speak i:nth-of-type(1)").hide();
+        $("footer .speak i:nth-of-type(2)").show();
+    })
+    $("footer input").focusout(function(){
+        $("footer .speak i:nth-of-type(1)").show();
+        $("footer .speak i:nth-of-type(2)").hide();
+    })
 
     function aggiungi(){
         if(event.which == 13){
@@ -11,31 +29,28 @@ $(document).ready(function() {
             $("main").append(copia);
 
             setTimeout(reply, 1000)
-
-            function reply(){
-
-                var answerArr = [
-                    "ciao",
-                    "come stai",
-                    "a presto"
-                ]
-
-                var answer = $(".template .in").clone();
-                answer.find(".text").append("<p>"+ answerArr[numRand(0,2)] +"</p>")
-                $("main").append(answer);
-            }
         };
+    }
+
+    function reply(){
+
+        var answerArr = [
+            "ciao",
+            "come stai",
+            "a presto"
+        ]
+
+        var answer = $(".template .in").clone();
+        answer.find(".text").append("<p>"+ answerArr[numRand(0,2)] +"</p>")
+        $("main").append(answer);
     }
 
     function numRand(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
     }
 
-
-
     var data = new Date();
     var ora = data.getHours() + ":" + data.getMinutes();
-
-
+    console.log(ora);
 
 });
