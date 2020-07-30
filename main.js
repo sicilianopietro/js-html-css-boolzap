577c6624cedc432259121814d0bcb58414ae7be1
pietro siciliano <siciliano.pietro@gmail.com>
2020-07-30 17:48:48 +0200

    k

--- main.js
+++ main.js
@@ -4,9 +4,10 @@ $(document).ready(function() {
         var valore = $("footer input").val();
         $("footer input").val("");
         var copia = $(".template .out").clone();
-        copia.find(".text").append("<p>" + valore + "</p>");
-        $("main").append(copia);
-        var x = $("main .out .text").find("p").attr("data-descr", orario());
+        copia.find(".text").append("<p>"+valore+"</p>");
+        // copia.find("p").append(valore);
+        $("main .main-chat.active").append(copia);
+        $("main .out .text").find("p").attr("data-descr", orario());
         scrollChat();
         setTimeout(reply, 1000);
     })
@@ -27,16 +28,25 @@ $(document).ready(function() {
 
     $("header .contact").find("span:nth-of-type(2)").append(orario());
 
+    $("aside .chat .contact").click(function(){
+        var index = $(this).index();
+        $("main .main-chat").removeClass("active");
+        $("aside .chat .contact").removeClass("active");
+        $(this).addClass("active");
+        $("main .main-chat").eq(index).addClass("active");
+    })
+
     function aggiungi(){
         if(event.which == 13){
             var valore = $("footer input").val();
             $("footer input").val("");
             var copia = $(".template .out").clone();
             copia.find(".text").append("<p>"+valore+"</p>");
-            $("main").append(copia);
-            var x = $("main .out .text").find("p").attr("data-descr", orario());
+            // copia.find("p").append(valore);
+            $("main .main-chat.active").append(copia);
+            $("main .out .text").find("p").attr("data-descr", orario());
             scrollChat();
-            setTimeout(reply, 1000)
+            setTimeout(reply, 1000);
         };
     }
 
@@ -50,13 +60,14 @@ $(document).ready(function() {
         var answerArr = [
             "ciao",
             "come stai",
-            "a presto"
+            "a presto",
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
         ]
 
         var answer = $(".template .in").clone();
-        answer.find(".text").append("<p>"+ answerArr[numRand(0,2)] +"</p>");
-        $("main").append(answer);
-        var x = $("main .in .text").find("p").attr("data-descr", orario());
+        answer.find(".text").append("<p>"+ answerArr[numRand(0,3)] +"</p>");
+        $("main .main-chat.active").append(answer);
+        $("main .in .text").find("p").attr("data-descr", orario());
         scrollChat();
     }
 
